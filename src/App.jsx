a8f2534be8b542cd08eb0dad69726562d5edc845
Fernando Bosco <fbosco@dlocal.com>
2022-06-13 18:39:02 -0300

    deu certo

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,29 @@ import './App.css'
 
 function App() {
 
+  let products = [
+    {name: 'Tennis listrado', price:  '240'},
+    {name: 'Sapato estiloso', price:  '400'},
+    {name: 'Chinelo branco', price:  '100'},
+    {name: 'Sapatilha laranja', price:  '250'},
+    {name: 'Pantufa fofa rosa', price:  '900'},
+    // {name: 'Moleton', price:  '430'}
+]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Site da Agencia</p>
         <p> Esse é um texto da agencia</p>
+        {products.map( (product) => {
+          return (
+            <div>
+
+             <p>{product.name}</p>
+             <p>R${product.price}</p>
+            </div>
+          )
+        } )}
       </header>
     </div>
   )
